cb8d120389e6fb0d8b3ebd5a09c2c4c188751927
Hongyu Huang <hongyuhuang963@gmail.com>
2023-04-20 16:36:59 +1200

    Converted components to styled-components

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,17 +1,38 @@
-import React from 'react';
-import './App.css';
-import Login from './Login';
+import React from "react";
+import "./App.css";
+import Login from "./Login";
+import styled from "styled-components";
 
+const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  font-family: "Open Sans", sans-serif;
+`;
+
+const Header = styled.header`
+  background: var(--otago-blue-light);
+  color: white;
+  padding: 0.5em;
+  height: 2.5em;
+  font-size: 2em;
+`;
+
+const Main = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  padding: 10px;
+  justify-content: center;
+`;
 function App() {
   return (
-    <div className="App">
-        <header>
-            EventCalendar
-        </header>
-        <main>
-            <Login/>
-        </main>
-    </div>
+    <Wrapper className="App">
+      <Header>EventCalendar</Header>
+      <Main>
+        <Login />
+      </Main>
+    </Wrapper>
   );
 }
 
