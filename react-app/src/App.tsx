30117d71c171f521742a5663aefa6c664dc55440
Hongyu Huang <hongyuhuang963@gmail.com>
2023-04-23 16:39:41 +1200

    Added code to route between different pages

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import "./App.css";
 import Login from "./Login";
 import styled from "styled-components";
+import { Routes, Route, Link } from "react-router-dom";
+import Home from "./Events";
+import About from "./About";
+import Events from "./Events";
 
 const Wrapper = styled.div`
   display: flex;
@@ -10,14 +14,20 @@ const Wrapper = styled.div`
 `;
 
 const Header = styled.header`
-  background: var(--otago-blue-light);
+  position: fixed;
+  display: flex;
+  align-items: center;
+  top: 0;
+  left: 0;
+  background: var(--otago-blue-dark);
   color: white;
-  padding: 0.5em;
-  height: 2.5em;
+  height: 64px;
   font-size: 2em;
+  width: 100%;
 `;
 
 const Main = styled.main`
+  margin-top: 64px;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -25,15 +35,37 @@ const Main = styled.main`
   padding: 10px;
   justify-content: center;
 `;
-function App() {
+
+const NavBar = styled.nav``;
+
+const App: React.FC = () => {
   return (
     <Wrapper className="App">
-      <Header>EventCalendar</Header>
+      <Header>
+        <span>Event Calendar</span>
+      </Header>
       <Main>
-        <Login />
+        <nav>
+          <ul>
+            <li>
+              <Link to={"/"}> Login </Link>
+            </li>
+            <li>
+              <Link to={"/events"}>Events</Link>
+            </li>
+            <li>
+              <Link to={"/about"}>About</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
       </Main>
     </Wrapper>
   );
-}
+};
 
 export default App;
