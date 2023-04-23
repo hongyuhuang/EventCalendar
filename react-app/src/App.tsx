46f84d578fb805c999939834dd7cf55dcdd7ff01
Hongyu Huang <hongyuhuang963@gmail.com>
2023-04-23 17:55:30 +1200

    Styled navigation bar and edited logo

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -3,7 +3,6 @@ import "./App.css";
 import Login from "./Login";
 import styled from "styled-components";
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./Events";
 import About from "./About";
 import Events from "./Events";
 
@@ -44,14 +43,36 @@ const Title = styled.span`
   padding-left: 10px;
 `;
 
-const Main = styled.main`
+const NavBar = styled.nav`
+  position: absolute;
+  background: var(--otago-grey-dark);
+  width: 100%;
   margin-top: 70px;
+  top: 0;
+  left: 0;
+`;
+
+const NavLink = styled(Link)`
+  color: var(--otago-blue-dark);
+  text-decoration: none;
+  font-size: 15px;
+  &:hover {
+    background: var(--otago-blue);
+    color: white;
+  }
+  height: 100%;
+  float: left;
+  padding: 7px;
+`;
+
+const Main = styled.main`
   display: flex;
   flex-direction: column;
   align-items: center;
   gap: 10px;
   padding: 10px;
   justify-content: center;
+  margin-top: 108.5px;
 `;
 
 const App: React.FC = () => {
@@ -63,21 +84,13 @@ const App: React.FC = () => {
           alt="University of Otago Logo"
         />
         <Title>Event Calendar</Title>
+        <NavBar>
+          <NavLink to={"/"}> Login </NavLink>
+          <NavLink to={"/events"}>Events </NavLink>
+          <NavLink to={"/about"}>About </NavLink>
+        </NavBar>
       </Header>
       <Main>
-        <nav>
-          <ul>
-            <li>
-              <Link to={"/"}> Login </Link>
-            </li>
-            <li>
-              <Link to={"/events"}>Events</Link>
-            </li>
-            <li>
-              <Link to={"/about"}>About</Link>
-            </li>
-          </ul>
-        </nav>
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/events" element={<Events />} />
