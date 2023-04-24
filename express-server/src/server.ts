119f1f3b69ebf8ab27b474ab3c4d52556272fc1f
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-04-24 20:21:59 +1200

    Route to get all users

--- express-server/src/server.ts
+++ express-server/src/server.ts
@@ -371,6 +371,22 @@ app.get("/user/:id", async (req, res) => {
 });
 
 /**
+ * Route to get all users
+ */
+app.get("/user", async (req, res) => {
+    try {
+        const [results] = await pool.query<User[]>(
+            `SELECT *
+             FROM USER`
+        );
+        res.send(results);
+    } catch (err) {
+        console.log(err);
+        res.status(500).send("An error occurred while getting the users");
+    }
+});
+
+/**
  * Returns a photo for a particular user.
  */
 app.get("/user/:username/photo", (req, res) => {});
