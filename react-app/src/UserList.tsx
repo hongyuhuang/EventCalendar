d435a518f20f32dfa8d8146eb3c78017a59c948e
Hongyu Huang <hongyuhuang963@gmail.com>
2023-04-25 16:04:39 +1200

    Added authentication

--- react-app/src/UserList.tsx
+++ react-app/src/UserList.tsx
@@ -1,40 +1,51 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 
-
 export interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
+    id: number;
+    firstName: string;
+    lastName: string;
+    email: string;
 }
 
 function UserList() {
-  const [users, setUsers] = useState<User[]>([]);
+    const [users, setUsers] = useState<User[]>([]);
 
-  useEffect(() => {
-    axios.get("/user")
-      .then((response) => {
-        console.log(response.data);
-        setUsers(response.data);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  }, [users]); 
+    const authHeader = (username: string, password: string) => {
+        const base64Credentials = btoa(`${username}:${password}`);
+        return `Basic ${base64Credentials}`;
+    };
+    const username = "johndoe@email.com";
+    const password = "password123";
 
-  return (
-    <div>
-      <h2>User List</h2>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.firstName} {user.lastName} - {user.email}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
+    const headers = {
+        Authorization: authHeader(username, password)
+      };
+
+    useEffect(() => {
+        axios
+            .get("http://localhost:3001/user", { headers })
+            .then((response) => {
+                console.log(response.data);
+                setUsers(response.data);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }, []);
+
+    return (
+        <div>
+            <h2>User List</h2>
+            <ul>
+                {users.map((user) => (
+                    <li key={user.id}>
+                        {user.firstName} {user.lastName} - {user.email}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+}
 
 export default UserList;
