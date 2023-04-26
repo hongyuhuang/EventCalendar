1fca8526e202856e0cdfcb2432a08a2c7e9900b8
airco879 <airco879@student.otago.ac.nz>
2023-04-26 21:16:14 +1200

    added test for get user's events

--- express-server/test/server.test.ts
+++ express-server/test/server.test.ts
@@ -80,6 +80,8 @@ describe("Test POST /event/:eventId/assign/:userId", () => {
   //         }
   //     })
   // })
+
+  
   test("Test for an event that doesn't exist", async () => {
     const nonExistentEventId = "non-existent-id";
     await axios.post(`http://localhost:3001/event/${nonExistentEventId}/assign/1`, {}, { headers })
@@ -203,169 +205,154 @@ describe("Test GET /user/:userId/events", () => {
 
 
     await axios.get(`http://localhost:3001/user/1/events`, { headers: headers })
-    .then((response) => {
-      //check valid
-      expect(response.status).toBe(200)
-      expect(response.data).toEqual([
-        {
-          ...event1,
-          eventId:1,
-          startDate:"2023-07-14T23:00:00.000Z", //adjusted for timezone conversion, I don't have time to fix otherwise
-          endDate:"2023-07-15T05:00:00.000Z"
-        },{
-          ...event2,
-          eventId:2,
-          startDate: "2023-09-11T21:00:00.000Z", //adjusted for timezone conversion, I don't have time to fix otherwise
-          endDate: "2023-09-14T05:00:00.000Z"
-        }])
-    })
+      .then((response) => {
+        //check valid
+        expect(response.status).toBe(200)
+        expect(response.data).toEqual([
+          {
+            ...event1,
+            eventId: 1,
+            startDate: "2023-07-14T23:00:00.000Z", //adjusted for timezone conversion, I don't have time to fix otherwise
+            endDate: "2023-07-15T05:00:00.000Z"
+          }, {
+            ...event2,
+            eventId: 2,
+            startDate: "2023-09-11T21:00:00.000Z", //adjusted for timezone conversion, I don't have time to fix otherwise
+            endDate: "2023-09-14T05:00:00.000Z"
+          }])
+      })
   });
 
-  test("Test for a user that does not exist", async () => { 
+  test("Test for a user that does not exist", async () => {
     await axios
-    .get("http://localhost:3001/user/zzzz/events", { headers: headers })
-    .catch((error) => {
-      expect(error.response.status).toBe(404);
-    });
-  
+      .get("http://localhost:3001/user/zzzz/events", { headers: headers })
+      .catch((error) => {
+        expect(error.response.status).toBe(404);
+      });
+
   });
 
-  // test("Test normally", async () => {
-  //   // //create user
-  //   const user = {
-  //     firstName: "Morty2",
-  //     lastName: "Smith2",
-  //     email: "morty@earth2.com",
-  //     isAdmin: false,
-  //     password: "123456"
-  //   }
-
-  //   const userResponse = await axios.post("http://localhost:3001/user", user, { headers: headers })
-  //   const userId = userResponse.data.userId
-
-  //   // //setup events
-  //   const event1: EventType = {
-  //     title: "Test event 1",
-  //     location: "test location",
-  //     startDate: new Date("2020-01-01T01:00:00.000Z"),
-  //     endDate: new Date("2020-01-01 02:00:00"),
-  //     description: "test description"
-  //   }
-  //   const event2: EventType = {
-  //     title: "Test event 2",
-  //     location: "test location",
-  //     startDate: new Date("2020-01-01 02:00:00"),
-  //     endDate: new Date("2020-01-01 03:00:00"),
-  //     description: "test description"
-  //   }
-  //   const event3: EventType = {
-  //     title: "Test event 3",
-  //     location: "test locat",
-  //     startDate: new Date("2020-01-01 03:00:00"),
-  //     endDate: new Date("2020-01-01 04:00:00"),
-  //     description: "test descprion"
-  //   }
-
-  //   var eventId1
-  //   var eventId2
-  //   var eventId3
-
-
-
-  //   // Create the test events
-  //   await axios.post("http://localhost:3001/event", event1, { headers: headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(201);
-  //       eventId1 = response.data.eventId;
-        
-  //     });
-
-  //   await axios.post("http://localhost:3001/event", event2, { headers: headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(201);
-  //       eventId2 = response.data.eventId;
-  //     });
-
-  //   await axios.post("http://localhost:3001/event", event3, { headers: headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(201);
-  //       eventId3 = response.data.eventId;
-  //     });
-
-  //     console.log(`${eventId1}, ${eventId2}, ${eventId3}`)
-  //     console.log(`${userId}`)
-    
-  //   //assign user to events
-
-
-  //   await axios.post(`http://localhost:3001/event/${eventId2}/assign/${userId}`, {headers: headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(201);
-  //     });
-
-  //   await axios.post(`http://localhost:3001/event/${eventId3}/assign/${userId}`, {headers: headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(201);
-  //     });
-
-  //   console.log("HELLO I GOT HERE")
-
-  //   await axios.get(`http://localhost:3001/user/${userId}/events`, { headers: headers })
-  //     .then((response) => {
-  //       //check valid
-  //       expect(response.status).toBe(200)
-  //       expect(response.data).toEqual([
-  //         {
-  //           ...event1,
-  //           eventId: eventId1
-  //         },
-  //         {
-  //           ...event2,
-  //           eventId: eventId2
-  //         },
-  //         {
-  //           ...event3,
-  //           eventId: eventId3
-  //         }
-  //       ])
-  //     })
-
-  //   console.log("GOT TO DELETE")
-
-  //   // Delete the test events
-  //   await axios
-  //     .delete(`http://localhost:3001/event/${eventId1}`, { headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(200);
-  //     });
-
-  //   await axios
-  //     .delete(`http://localhost:3001/event/${eventId1}`, { headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(200);
-  //     });
-
-  //   await axios
-  //     .delete(`http://localhost:3001/event/${eventId1}`, { headers })
-  //     .then((response) => {
-  //       expect(response.status).toBe(200);
-  //     });
-
-  //   await axios
-  //     .delete(`http://localhost:3001/user/${userId}`, { headers: headers })
-  //     .then((response) => {
-  //       // console.log("Status", response.status);
-  //       expect(response.status).toBe(204);
-  //     });
-
-  //   // //delete user and events
-  //   // await request.delete(`/event/${eventId1}`).expect(204)
-  //   // await request.delete(`/event/${eventId2}`).expect(204)
-  //   // await request.delete(`/event/${eventId3}`).expect(204)
-  //   // await request.delete(`/user/${userId}`).expect(204)
-  // });
-
+  test("Test normally", async () => {
+    // create user
+    const user = {
+      firstName: "Morty2",
+      lastName: "Smith2",
+      email: "morty@earth2.com",
+      isAdmin: false,
+      password: "123456"
+    }
   
+    const userResponse = await axios.post("http://localhost:3001/user", user, {
+      headers: headers
+    })
+    const userId = userResponse.data.userId
+  
+    // setup events
+    const startDate = new Date(new Date().setMilliseconds(0))
+    const endDate = new Date(new Date(startDate.getTime() + 60 * 60 * 1000).setMilliseconds(0)) // add 1 hour to startDate
+    const event1: EventType = {
+      title: "Test event 1",
+      location: "test location 1",
+      startDate: startDate,
+      endDate: endDate,
+      description: "test description 1"
+    }
+    const event2: EventType = {
+      title: "Test event 2",
+      location: "test location 2",
+      startDate: startDate,
+      endDate: endDate,
+      description: "test description 2"
+    }
+    const event3: EventType = {
+      title: "Test event 3",
+      location: "test location 3",
+      startDate: startDate,
+      endDate: endDate,
+      description: "test description 3"
+    }
+  
+    let eventId1
+    let eventId2
+    let eventId3
+  
+    // Create the test events
+    await axios.post("http://localhost:3001/event", event1, { headers: headers })
+      .then((response) => {
+        expect(response.status).toBe(201);
+        eventId1 = response.data.eventId;
+  
+      });
+  
+    await axios.post("http://localhost:3001/event", event2, { headers: headers })
+      .then((response) => {
+        expect(response.status).toBe(201);
+        eventId2 = response.data.eventId;
+      });
+  
+    await axios.post("http://localhost:3001/event", event3, { headers: headers })
+      .then((response) => {
+        expect(response.status).toBe(201);
+        eventId3 = response.data.eventId;
+      });
+  
+    // assign user to events
+    await axios.post(`http://localhost:3001/event/${eventId2}/assign/${userId}`, {}, { headers: headers })
+      .then((response) => {
+        expect(response.status).toBe(201);
+      });
+  
+    await axios.post(`http://localhost:3001/event/${eventId3}/assign/${userId}`, {}, { headers: headers })
+      .then((response) => {
+        expect(response.status).toBe(201);
+      });
+  
+    await axios.get(`http://localhost:3001/user/${userId}/events`, { headers: headers })
+      .then((response) => {
+        console.log(response.data)
+        expect(response.status).toBe(200);
+        expect(response.data[0]).toStrictEqual({
+          ...event2,
+          eventId: eventId2,
+          startDate: startDate.toISOString(),
+          endDate: endDate.toISOString()
+        });
+        expect(response.data[1]).toStrictEqual({
+          ...event3,
+          eventId: eventId3,
+          startDate: startDate.toISOString(),
+          endDate: endDate.toISOString()
+        });
+      });
+
+    // Delete the test events
+    await axios
+      .delete(`http://localhost:3001/event/${eventId1}`, { headers })
+      .then((response) => {
+        expect(response.status).toBe(200);
+      });
+
+    await axios
+      .delete(`http://localhost:3001/event/${eventId2}`, { headers })
+      .then((response) => {
+        expect(response.status).toBe(200);
+      });
+
+    await axios
+      .delete(`http://localhost:3001/event/${eventId3}`, { headers })
+      .then((response) => {
+        expect(response.status).toBe(200);
+      });
+
+    await axios
+      .delete(`http://localhost:3001/user/${userId}`, { headers: headers })
+      .then((response) => {
+        // console.log("Status", response.status);
+        expect(response.status).toBe(204);
+      });
+  });
+
+
 });
 
 describe("Test /user", () => {
