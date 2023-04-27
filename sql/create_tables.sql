770d65ffd1e8bfb0ad68ee0d65247148ee6d9993
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-04-28 11:23:26 +1200

    Adding uniqueness constraint to userId, eventId in attendance record. Now can't have duplicate assignments. Important change for CRUD of assignments, since (userId, eventId) is not the PK

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,40 +1,46 @@
-CREATE TABLE USER(
-    userId INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE USER
+(
+    userId    INT          NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(100) NOT NULL,
-    lastName VARCHAR(100) NOT NULL,
-    isAdmin BOOLEAN NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
+    lastName  VARCHAR(100) NOT NULL,
+    isAdmin   BOOLEAN      NOT NULL,
+    email     VARCHAR(100) NOT NULL,
+    password  VARCHAR(100) NOT NULL,
     PRIMARY KEY (userId),
     UNIQUE (email)
 );
 
-CREATE TABLE EVENT(
-    eventId INT NOT NULL AUTO_INCREMENT,
-    title VARCHAR(100) NOT NULL,
-    location VARCHAR(100) NOT NULL,
-    startDate DATETIME NOT NULL,
-    endDate DATETIME NOT NULL,
+CREATE TABLE EVENT
+(
+    eventId     INT           NOT NULL AUTO_INCREMENT,
+    title       VARCHAR(100)  NOT NULL,
+    location    VARCHAR(100)  NOT NULL,
+    startDate   DATETIME      NOT NULL,
+    endDate     DATETIME      NOT NULL,
     description VARCHAR(1000) NOT NULL,
     PRIMARY KEY (eventId)
 );
 
-CREATE TABLE ATTENDANCE_RECORD(
+CREATE TABLE ATTENDANCE_RECORD
+(
     attendanceId INT NOT NULL AUTO_INCREMENT,
-    userId INT NOT NULL,
-    eventId INT NOT NULL,
+    userId       INT NOT NULL,
+    eventId      INT NOT NULL,
     PRIMARY KEY (attendanceId),
-    FOREIGN KEY (userId) REFERENCES USER(userId),
-    FOREIGN KEY (eventId) REFERENCES EVENT(eventId) ON DELETE CASCADE
+    FOREIGN KEY (userId) REFERENCES USER (userId),
+    FOREIGN KEY (eventId) REFERENCES EVENT (eventId) ON DELETE CASCADE,
+    UNIQUE (userId, eventId)
 );
 
-CREATE TABLE EVENT_SCHEDULE_ITEM(
-    scheduleItemId INT NOT NULL AUTO_INCREMENT,
-    eventId INT NOT NULL,
-    startDate DATETIME NOT NULL,
-    endDate DATETIME NOT NULL,
-    activity VARCHAR(100) NOT NULL,
-    description VARCHAR(1000) NOT NULL,
+
+CREATE TABLE EVENT_SCHEDULE_ITEM
+(
+    scheduleItemId INT           NOT NULL AUTO_INCREMENT,
+    eventId        INT           NOT NULL,
+    startDate      DATETIME      NOT NULL,
+    endDate        DATETIME      NOT NULL,
+    activity       VARCHAR(100)  NOT NULL,
+    description    VARCHAR(1000) NOT NULL,
     PRIMARY KEY (scheduleItemId),
-    FOREIGN KEY (eventId) REFERENCES EVENT(eventId)
+    FOREIGN KEY (eventId) REFERENCES EVENT (eventId)
 );
