e34e11f46817112dc158bda90069789ac754523f
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-04-29 13:32:47 +1200

    adding recaptcha to login route.
    took wayyy longer than i thought

--- express-server/src/express/auth.ts
+++ express-server/src/express/auth.ts
@@ -3,9 +3,18 @@ import assert from "assert";
 import acl from "express-acl";
 import { Pool } from "mysql2/promise";
 import { User } from "../entities";
+const bodyParser = require("body-parser");
 
 const pool: Pool = require("../sql-setup").pool;
 const authRouter = require("express").Router();
+
+// Set up reCaptcha for login
+const ReCaptcha = require("express-recaptcha").RecaptchaV2;
+const reCaptcha = new ReCaptcha(
+    process.env.RECAPTCHA_SITE_KEY,
+    process.env.RECAPTCHA_SECRET
+);
+authRouter.use(bodyParser.urlencoded({ extended: true }));
 
 authRouter.use(
     basicAuth({
@@ -109,12 +118,35 @@ authRouter.use(acl.authorize);
  *
  * Basically just returns whether a user is an admin or not that a user has, given the headers. It is already assumed that their credentials are valid.
  */
-authRouter.get("/login", (req, res) => {
-    res.status(200).json({
+authRouter.get("/login", reCaptcha.middleware.verify, (req, res) => {
+    console.log(req.recaptcha);
+    if (!req.recaptcha || req.recaptcha.error) {
+        console.log(req.recaptcha);
+        console.log(req.query);
+        return res.status(422).send("Failed reCAPTCHA");
+    }
+    return res.status(200).json({
         // @ts-ignore
         isAdmin: req.role === "admin",
     });
 });
+
+/**
+ * General middleware function for reCaptcha
+ *
+ * Sends a 422 if the reCaptcha fails
+ *
+ * @param req express request
+ * @param res express response
+ * @param next express next function
+ */
+async function reCaptchaMiddleware(req, res, next) {
+    await reCaptcha.middleware.verify(req, res, next);
+    if (!req.recaptcha || req.recaptcha.error) {
+        return res.status(422).send("Failed reCAPTCHA");
+    }
+    next();
+}
 
 module.exports = {
     authRouter,
