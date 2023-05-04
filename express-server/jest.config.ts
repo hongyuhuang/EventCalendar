f9a4e4d3344e3add2df2528c1faa55add0791020
Hugo Phibbs <81545721+HugoPhibbs@users.noreply.github.com>
2023-05-04 14:04:28 +1200

    Fixing jest setup file

--- express-server/jest.config.ts
+++ express-server/jest.config.ts
@@ -1,23 +1,12 @@
-
-
-export default {
-
-  clearMocks: true,
-
-  collectCoverage: true,
-
-  coverageDirectory: "coverage",
-
-  coverageProvider: "v8",
-
-  preset: "ts-jest"
-};
-
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.tsx?$': 'ts-jest',
-    '^.+\\.jsx?$': 'babel-jest',
-  }
+    clearMocks: true,
+    collectCoverage: true,
+    coverageDirectory: "coverage",
+    coverageProvider: "v8",
+    preset: "ts-jest",
+    testEnvironment: "node",
+    transform: {
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.jsx?$": "babel-jest",
+    },
 };
