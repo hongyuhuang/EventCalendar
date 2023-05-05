ceaca8c29533168b525bb24c7aeacacdedad7a80
Hongyu Huang <hongyuhuang963@gmail.com>
2023-05-05 22:27:12 +1200

    Implemented logic for dynamic nav bar

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -2,12 +2,7 @@ import React from "react";
 import "./App.css";
 import Login from "./Login";
 import styled from "styled-components";
-import {
-  Routes,
-  Route,
-  NavLink,
-  useLocation,
-} from "react-router-dom";
+import { Routes, Route, NavLink, useLocation } from "react-router-dom";
 import EventCalendar from "./EventCalendar";
 import SignupForm from "./AddUser";
 import EventDetails from "./EventDetails";
@@ -15,139 +10,159 @@ import CreateEventForm from "./AddEvent";
 import UserList from "./UserList";
 
 const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-family: "Open Sans", sans-serif;
+    display: flex;
+    flex-direction: column;
+    font-family: "Open Sans", sans-serif;
 `;
 
 const Header = styled.header`
-  &::before {
-    content: "";
-    display: block;
-    position: absolute;
+    &::before {
+        content: "";
+        display: block;
+        position: absolute;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 3px;
+        background-color: var(--otago-yellow);
+    }
+    position: fixed;
+    display: flex;
+    align-items: center;
     top: 0;
     left: 0;
+    background: var(--otago-blue-dark);
+    color: white;
+    height: 70px;
+    font-size: 24px;
     width: 100%;
-    height: 3px;
-    background-color: var(--otago-yellow);
-  }
-  position: fixed;
-  display: flex;
-  align-items: center;
-  top: 0;
-  left: 0;
-  background: var(--otago-blue-dark);
-  color: white;
-  height: 70px;
-  font-size: 24px;
-  width: 100%;
-  z-index: 9999;
+    z-index: 9999;
 `;
 
 const Logo = styled.img`
-  height: 70px;
+    height: 70px;
 `;
 
 const Title = styled.span`
-  padding-left: 10px;
+    padding-left: 10px;
 `;
 
 const Nav = styled.nav`
-  position: absolute;
-  background: var(--otago-grey-dark);
-  width: 100%;
-  margin-top: 70px;
-  top: 0;
-  left: 0;
+    position: absolute;
+    background: var(--otago-grey-dark);
+    width: 100%;
+    margin-top: 70px;
+    top: 0;
+    left: 0;
 `;
 
 const NavItem = styled(NavLink)`
-  color: var(--otago-blue);
-  text-decoration: none;
-  font-size: 15px;
-  padding: 10px;
-  &.active {
-    background-color: white;
-    color: var(--otago-blue-dark);
-  }
-  &:hover {
-    background: var(--otago-blue);
-    color: white;
-  }
-  height: 100%;
-  float: left;
+    color: var(--otago-blue);
+    text-decoration: none;
+    font-size: 15px;
+    padding: 10px;
+    &.active {
+        background-color: white;
+        color: var(--otago-blue-dark);
+    }
+    &:hover {
+        background: var(--otago-blue);
+        color: white;
+    }
+    height: 100%;
+    float: left;
 `;
 
 const Main = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  gap: 10px;
-  padding: 10px;
-  justify-content: center;
-  margin-top: 108.5px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 10px;
+    padding: 10px;
+    justify-content: center;
+    margin-top: 108.5px;
 `;
 
 const App: React.FC = () => {
-  const location = useLocation();
-  const currentPath = location.pathname;
+    const location = useLocation();
+    const currentPath = location.pathname;
+    const userRole = "admin"; // TODO: Replace this with the actual user role
+    const loggedIn: boolean = true; // TODO: Replace this with the actual logged in state
 
-  const isLinkActive = (linkPath: string) => {
-    return linkPath === "/"
-      ? currentPath === linkPath
-      : currentPath.startsWith(linkPath);
-  };
+    const isLinkActive = (linkPath: string) => {
+        return linkPath === "/"
+            ? currentPath === linkPath
+            : currentPath.startsWith(linkPath);
+    };
 
-  return (
-    <Wrapper>
-      <Header>
-        <Logo
-          src={"/images/university-of-otago-logo.png"}
-          alt="University of Otago Logo"
-        />
-        <Title>Event Calendar</Title>
-        <Nav>
-          <NavItem to={"/"} className={isLinkActive("/") ? "active" : ""}>
-            Login
-          </NavItem>
-          <NavItem
-            to={"/events"}
-            className={isLinkActive("/events") ? "active" : ""}
-          >
-            Events
-          </NavItem>
-          <NavItem
-            to={"/add-user"}
-            className={isLinkActive("/add-user") ? "active" : ""}
-          >
-            Add User
-          </NavItem>
-          <NavItem
-            to={"/user-list"}
-            className={isLinkActive("/user-list") ? "active" : ""}
-          >
-            Manage Users
-          </NavItem>
-          <NavItem
-            to={"/add-event"}
-            className={isLinkActive("/add-event") ? "active" : ""}
-          >
-            Add Event
-          </NavItem>
-        </Nav>
-      </Header>
-      <Main>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/events" element={<EventCalendar />} />
-          <Route path="/add-user" element={<SignupForm />} />
-          <Route path="/user-list" element={<UserList />} />
-          <Route path="/event-details" element={<EventDetails />}></Route>
-          <Route path="/add-event" element={<CreateEventForm />}></Route>
-        </Routes>
-      </Main>
-    </Wrapper>
-  );
+    return (
+        <Wrapper>
+            <Header>
+                <Logo
+                    src={"/images/university-of-otago-logo.png"}
+                    alt="University of Otago Logo"
+                />
+                <Title>Event Calendar</Title>
+                <Nav>
+                    {loggedIn === true && (
+                        <>
+                            <NavItem
+                                to={"/events"}
+                                className={
+                                    isLinkActive("/events") ? "active" : ""
+                                }
+                            >
+                                Events
+                            </NavItem>
+                            {userRole === "admin" && ( <>
+                              <NavItem
+                                to={"/add-event"}
+                                className={
+                                    isLinkActive("/add-event") ? "active" : ""
+                                }
+                            >
+                                Add Event
+                            </NavItem>
+                            <NavItem
+                                to={"/user-list"}
+                                className={
+                                    isLinkActive("/user-list") ? "active" : ""
+                                }
+                            >
+                                Manage Users
+                            </NavItem>
+                            <NavItem
+                                to={"/add-user"}
+                                className={
+                                    isLinkActive("/add-user") ? "active" : ""
+                                }
+                            >
+                                Add User
+                            </NavItem>
+                            </>)}
+                        </>
+                    )}
+                </Nav>
+            </Header>
+            <Main>
+                <Routes>
+                    <Route path="/" element={<Login />} />
+                    <Route path="/events" element={<EventCalendar />} />
+                    {userRole === "admin" && (
+                        <>
+                            <Route path="/add-user" element={<SignupForm />} />
+                            <Route
+                                path="/add-event"
+                                element={<CreateEventForm />}
+                            />
+                            <Route path="/user-list" element={<UserList />} />
+                        </>
+                    )}
+                    <Route path="/event-details" element={<EventDetails />} />
+                </Routes>
+            </Main>
+        </Wrapper>
+    );
 };
 
 export default App;
