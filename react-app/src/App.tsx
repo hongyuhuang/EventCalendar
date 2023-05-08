013aa8a975ca03e1e4eb54486e715840d0ae6504
Hongyu Huang <hongyuhuang963@gmail.com>
2023-05-08 15:58:50 +1200

    Deleted import

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Login from "./Login";
 import styled from "styled-components";
