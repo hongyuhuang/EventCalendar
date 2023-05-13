2de887d034cbad81ff8416a9ca615e9ae3f6951c
airco879 <airco879@student.otago.ac.nz>
2023-05-13 18:12:32 +1200

    added cronjob for deleting events automatically

---
+++ express-server/src/express/event-cleaner.ts
@@ -0,0 +1,60 @@
+const cron = require('node-cron');
+import { Pool, RowDataPacket } from "mysql2/promise";
+const { pool, handleDbError } = require("../helpers") as {
+    pool: Pool;
+    handleDbError: any;
+};
+
+async function deleteFinishedEvents() {
+    try {
+        // Fetch all events from the database
+        const [rows] = await pool.query("SELECT eventId, endDate FROM EVENT");
+        
+        const events = rows as { eventId: number, endDate: string }[];
+
+        const currentTime = new Date();
+
+        const eventsToDelete = [];
+
+        // Check each event's endDate and collect the IDs of events to be deleted
+        events.forEach(event => {
+            const { eventId, endDate } = event;
+            const formattedEndDate = new Date(endDate);
+
+            if (formattedEndDate <= currentTime) {
+                eventsToDelete.push(eventId);
+            }
+        });
+
+        // Delete the events from the database
+        if (eventsToDelete.length > 0) {
+            await pool.query("DELETE FROM EVENT WHERE eventId IN (?)", [eventsToDelete]);
+        }
+
+        const deleteCount = eventsToDelete.length;
+        // console.log("Deleting: " + deleteCount);
+        return deleteCount;
+    } catch (err) {
+        throw new Error("An error occurred while deleting the events: " + err);
+    }
+}
+
+function startCronJob() {
+    //every min: '* * * * *'  <- use for demo
+    //every hour '0 * * * *'
+    const task = cron.schedule('* * * * *', async () => {
+        console.log("attempting...")
+        try {
+            const deletedCount = await deleteFinishedEvents();
+            console.log(`${deletedCount} event(s) deleted`);
+        } catch (error) {
+            console.error('Error deleting events:', error);
+        }
+    });
+
+    task.start();
+}
+
+module.exports = {
+    startCronJob
+};
